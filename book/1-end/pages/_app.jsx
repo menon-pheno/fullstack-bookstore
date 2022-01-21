105b13c016dc3c176abf89db9684565c57d67d2f
pheno <pheno_the_best@yahoo.com.tw>
2022-01-21 19:11:29 +0800

    chapter 1

---
+++ book/1-end/pages/_app.jsx
@@ -0,0 +1,46 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
+import App from 'next/app';
+import React from 'react';
+import Head from 'next/head';
+import PropTypes from 'prop-types';
+
+import { theme } from '../lib/theme';
+
+import Header from '../components/Header';
+
+const propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired, // eslint-disable-line
+};
+
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentNode.removeChild(jssStyles);
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props;
+
+    // console.log(pageProps);
+
+    return (
+      <ThemeProvider theme={theme}>
+        <Head>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        </Head>
+        <CssBaseline />
+        <Header {...pageProps} />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    );
+  }
+}
+
+MyApp.propTypes = propTypes;
+
+export default MyApp;
