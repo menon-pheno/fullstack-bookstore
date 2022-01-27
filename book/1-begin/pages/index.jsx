87a9a0685f368670d7c62ac0d1c6d9d7c7bbf071
pheno <pheno_the_best@yahoo.com.tw>
2022-01-27 12:32:07 +0800

    index page

---
+++ book/1-begin/pages/index.jsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+const Index = () => (
+  <div style={{ padding: '10px 45px' }}>
+    <Head>
+      <title>首頁</title>
+      <meta name="說明" content="這是首頁的說明資訊" />
+    </Head>
+    <p>首頁資訊</p>
+  </div>
+);
+
+export default Index;
