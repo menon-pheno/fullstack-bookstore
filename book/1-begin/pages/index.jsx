29c3a756b0fe276f088b4862944e2e1b1d034a91
pheno <pheno_the_best@yahoo.com.tw>
2022-01-27 17:26:19 +0800

    remove index header

--- book/1-begin/pages/index.jsx
+++ book/1-begin/pages/index.jsx
@@ -1,14 +1,11 @@
 import Head from 'next/head';
 
-import Header from '../components/Header';
-
 const Index = () => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>首頁</title>
       <meta name="說明" content="這是首頁的說明資訊" />
     </Head>
-    <Header />
     <p>首頁資訊</p>
   </div>
 );
