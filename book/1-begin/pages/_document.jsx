bb21516c9d5fb2ab605e848b6992c99c08c0cd8e
pheno <pheno_the_best@yahoo.com.tw>
2022-01-28 12:46:00 +0800

    update styles

--- book/1-begin/pages/_document.jsx
+++ book/1-begin/pages/_document.jsx
@@ -1,4 +1,6 @@
+import React from 'react';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
+import { ServerStyleSheets } from '@material-ui/styles';
 
 export default class MyDocument extends Document {
   render() {
@@ -35,7 +37,7 @@ export default class MyDocument extends Document {
             {`
                 a {
                     font-weight: 400;
-                    color: #58aa6ff;
+                    color: #58age6ff;
                     text-decoration: none;
                     outline: none;
                 }
@@ -66,3 +68,51 @@ export default class MyDocument extends Document {
     );
   }
 }
+
+// `getInitialProps` 屬於 `_document` 而非 `_app`
+// 適用於伺服器端渲染（server-side generation, SSG）
+MyDocument.getInitialProps = async (ctx) => {
+  // 執行順序
+  //
+  // 伺服器端：
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. document.getInitialProps
+  // 4. app.render
+  // 5. page.render
+  // 6. document.render
+  //
+  // 伺服器端處理錯誤的時候：
+  // 1. document.getInitialProps
+  // 2. app.render
+  // 3. page.render
+  // 4. document.render
+  //
+  // 瀏覽器端：
+  // 1. app.getInitialProps
+  // 2. page.getInitialProps
+  // 3. app.render
+  // 4. page.render
+
+  // 渲染 app 以及 page 然後取得將副作用（side effect）集合起來的頁面 context
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // styles fragment 在 app 及 page 渲染完成後才會渲染
+    styles: (
+      <>
+        {initialProps.styles}
+        {sheets.getStyleElement()}
+      </>
+    ),
+  };
+};
