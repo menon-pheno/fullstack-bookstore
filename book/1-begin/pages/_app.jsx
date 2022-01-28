1e1ce59608bf1148f809dff18da37eb6c76fb4c3
pheno <pheno_the_best@yahoo.com.tw>
2022-01-28 15:44:25 +0800

    Mui Integration

--- book/1-begin/pages/_app.jsx
+++ book/1-begin/pages/_app.jsx
@@ -1,7 +1,10 @@
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/styles';
 import App from 'next/app';
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { theme } from '../lib/theme';
 import Header from '../components/Header';
 
 const propTypes = {
@@ -10,14 +13,23 @@ const propTypes = {
 };
 
 class MyApp extends App {
+  componentDidMount() {
+    // 移除伺服器端注入的 CSS
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles && jssStyles.parentNode) {
+      jssStyles.parentElement.removeChild(jssStyles);
+    }
+  }
+
   render() {
     const { Component, pageProps } = this.props;
 
     return (
-      <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
         <Header {...pageProps} />
         <Component {...pageProps} />
-      </>
+      </ThemeProvider>
     );
   }
 }
