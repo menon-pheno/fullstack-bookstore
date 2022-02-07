cd3c60f76f4960d1dc1f4029d7c6f617a9ae9a0b
pheno <pheno_the_best@yahoo.com.tw>
2022-02-07 21:06:32 +0800

    add mongoose

--- book/2-begin/server/server.js
+++ book/2-begin/server/server.js
@@ -1,10 +1,22 @@
 const express = require('express');
 const next = require('next');
+const mongoose = require('mongoose');
+
+const dev = process.env.NODE_ENV !== 'production';
+const MONGO_URL = process.env.MONGO_URL_TEST;
+
+const options = {
+  useNewUrlParse: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(MONGO_URL, options);
 
 const port = process.env.PORT || 8000;
 const ROOT_URL = `http://localhost:${port}`;
 
-const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
