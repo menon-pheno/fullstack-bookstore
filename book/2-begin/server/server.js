14ac4bfed7d1c42fb271c5c61af2df967a158e87
pheno <pheno_the_best@yahoo.com.tw>
2022-02-07 13:29:29 +0800

    express-next integration

--- book/2-begin/server/server.js
+++ book/2-begin/server/server.js
@@ -1,11 +1,25 @@
-const express = require("express");
+const express = require('express');
+const next = require('next');
 
-const server = express();
+const port = process.env.PORT || 8000;
+const ROOT_URL = `http://localhost:${port}`;
 
-server.get("/", (req, res) => {
-  res.send("我的 express 伺服器");
-});
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-server.listen(3000, () => {
-  console.log("Port 3000 準備就緒！"); // eslint-disable-line no-console
+app.prepare().then(() => {
+  const server = express();
+
+  server.get('/', (req, res) => {
+    res.send('我的 express 伺服器');
+  });
+
+  server.get('*', (req, res) => handle(req, res));
+
+  server.listen(port, (err) => {
+    if (err) throw err;
+    // eslint-disable-next-line no-console
+    console.log(`> 在 ${ROOT_URL} 上準備好了`);
+  });
 });
