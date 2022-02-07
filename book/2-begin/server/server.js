644bd0d1e49d2638bce5d11ffca19eea8e456619
pheno <pheno_the_best@yahoo.com.tw>
2022-02-07 14:16:51 +0800

    script for dev-express

--- book/2-begin/server/server.js
+++ book/2-begin/server/server.js
@@ -12,7 +12,7 @@ app.prepare().then(() => {
   const server = express();
 
   server.get('/', (req, res) => {
-    res.send('太棒了，Express 自動更新');
+    res.send('yarn dev-express: 太棒了，Express 自動更新');
   });
 
   server.get('*', (req, res) => handle(req, res));
