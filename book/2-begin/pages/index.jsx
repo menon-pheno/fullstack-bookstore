dc792e7503d50553c002dc37893e70ae85b7ef17
pheno <pheno_the_best@yahoo.com.tw>
2022-02-07 15:26:48 +0800

    initial props for index

--- book/2-begin/pages/index.jsx
+++ book/2-begin/pages/index.jsx
@@ -1,13 +1,34 @@
+import PropTypes from 'prop-types';
 import Head from 'next/head';
 
-const Index = () => (
+const propTypes = {
+  user: PropTypes.shape({
+    displayName: PropTypes.string,
+    email: PropTypes.string.isRequired,
+  }),
+};
+
+const defaultProps = {
+  user: null,
+};
+
+const Index = ({ user }) => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>首頁</title>
       <meta name="說明" content="這是首頁的說明資訊" />
     </Head>
     <p>首頁資訊</p>
+    <p>
+      Email:
+      {user.email}
+    </p>
   </div>
 );
+
+Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+
+Index.propTypes = propTypes;
+Index.defaultProps = defaultProps;
 
 export default Index;
