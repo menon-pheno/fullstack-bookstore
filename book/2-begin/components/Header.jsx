0e4dbbeb0a232885ec976c050ee459d8cf2aa823
pheno <pheno_the_best@yahoo.com.tw>
2022-02-14 12:02:48 +0800

    change Header

--- book/2-begin/components/Header.jsx
+++ book/2-begin/components/Header.jsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import Toolbar from '@material-ui/core/Toolbar';
 import Grid from '@material-ui/core/Grid';
+import Avatar from '@material-ui/core/Avatar';
 
 import MenuWithAvatar from './MenuWithAvatar';
 import { styleToolbar } from './SharedStyles';
@@ -21,6 +22,17 @@ const Header = ({ user }) => (
   <div>
     <Toolbar style={styleToolbar}>
       <Grid container direction="row" justify="space-around" align="center">
+        <Grid item sm={11} xs={9} style={{ textAlign: 'left' }}>
+          {user ? null : (
+            <Link href="/">
+              <Avatar
+                src="https://storage.googleapis.com/builderbook/logo.svg"
+                alt="Bookstore logo"
+                style={{ margin: '0px auto 0px 20px', cursor: 'pointer' }}
+              />
+            </Link>
+          )}
+        </Grid>
         <Grid item xs={12} style={{ textAlign: 'right' }}>
           {user ? (
             <div style={{ whiteSpace: ' nowrap' }}>
