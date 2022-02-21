8c4f39e48932ce15f757224a96c945ae7a2d6b27
pheno <pheno_the_best@yahoo.com.tw>
2022-02-21 12:45:12 +0800

    setupGoogle skeleton

--- book/3-begin/server/google.js
+++ book/3-begin/server/google.js
@@ -1,5 +1,18 @@
+const passport = require('passport');
+const Strategy = require('passport-google-oauth').OAuth2Strategy;
+
 function setupGoogle({ server, ROOT_URL }) {
-  // 接下來需要將 Google 認證相關步驟給實作出來
+  // 根據 passport 範例，我們先需要引入並且 use 這個套件
+  passport.use(
+    new Strategy(
+      {
+        clientID: process.env.GOOGLE_CLIENTID,
+        clientSecret: process.env.GOOGLE_CLIENTSECRET,
+        callbackURL: `${ROOT_URL}/oauth2callback`,
+      },
+      verify, // 接著需要定義跟實作這個函式
+    ),
+  );
 }
 
 module.exports = setupGoogle;
