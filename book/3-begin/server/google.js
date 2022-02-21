b27b72ce14398f5d5f164f5d5600dbba273f1e28
pheno <pheno_the_best@yahoo.com.tw>
2022-02-21 13:02:24 +0800

    setupGoogle skeleton

--- book/3-begin/server/google.js
+++ book/3-begin/server/google.js
@@ -2,6 +2,20 @@ const passport = require('passport');
 const Strategy = require('passport-google-oauth').OAuth2Strategy;
 
 function setupGoogle({ server, ROOT_URL }) {
+  // passport 所需要使用的 callback 函式
+  const verify = async (accessToken, refreshToken, profile, verified) => {
+    let email;
+    let avatarUrl;
+
+    if (profile.emails) {
+      email = profile.emails[0].value;
+    }
+
+    if (profile.photos && profile.photos.length > 0) {
+      avatarUrl = profile.photos[0].value.replace('sz=50', 'sz=128');
+    }
+  };
+
   // 根據 passport 範例，我們先需要引入並且 use 這個套件
   passport.use(
     new Strategy(
@@ -10,7 +24,7 @@ function setupGoogle({ server, ROOT_URL }) {
         clientSecret: process.env.GOOGLE_CLIENTSECRET,
         callbackURL: `${ROOT_URL}/oauth2callback`,
       },
-      verify, // 接著需要定義跟實作這個函式
+      verify,
     ),
   );
 }
