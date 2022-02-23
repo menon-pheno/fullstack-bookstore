906faf627d1f0863ec46301a1080ad8c10abe84e
pheno <pheno_the_best@yahoo.com.tw>
2022-02-23 21:33:58 +0800

    slugify

--- book/3-begin/server/utils/slugify.js
+++ book/3-begin/server/utils/slugify.js
@@ -2,7 +2,15 @@ const _ = require('lodash');
 
 const slugify = (text) => _.kebabCase(text);
 
-async function generateSlug() {
+async function generateSlug(Model, name, filter = {}) {
+  const origSlug = slugify(name);
+
+  const user = await Model.findOne({ slug: origSlug, ...filter }, 'id');
+
+  if (!user) {
+    return origSlug;
+  }
+
   return null;
 }
 
