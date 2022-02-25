9197e25b613607258975439253fd5d48fa755d28
pheno <pheno_the_best@yahoo.com.tw>
2022-02-25 18:45:31 +0800

    1-end

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,34 +1,15 @@
-import PropTypes from 'prop-types';
 import Head from 'next/head';
+import Button from '@material-ui/core/Button';
 
-const propTypes = {
-  user: PropTypes.shape({
-    displayName: PropTypes.string,
-    email: PropTypes.string.isRequired,
-  }),
-};
-
-const defaultProps = {
-  user: null,
-};
-
-const Index = ({ user }) => (
+const Index = () => (
   <div style={{ padding: '10px 45px' }}>
     <Head>
       <title>首頁</title>
       <meta name="說明" content="這是首頁的說明資訊" />
     </Head>
     <p>首頁資訊</p>
-    <p>
-      Email: testing...
-      {user.email}
-    </p>
+    <Button variant="contained">MUI 按鈕</Button>
   </div>
 );
-
-Index.getInitialProps = async (ctx) => ({ user: ctx.req.user });
-
-Index.propTypes = propTypes;
-Index.defaultProps = defaultProps;
 
 export default Index;
