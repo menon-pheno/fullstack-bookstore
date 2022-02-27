d8971050ec8167fe43fed0a06defda4cb9a320c9
John Huang <pheno.the.best@gmail.com>
2022-02-27 10:33:30 +0800

    start home development

--- pages/index.jsx
+++ pages/index.jsx
@@ -20,11 +20,14 @@ const Index = ({ user }) => (
       <meta name="說明" content="這是首頁的說明資訊" />
     </Head>
     <p>首頁資訊</p>
-    <Button variant="contained">MUI 按鈕</Button>
+    <Button variant="contained">MUI 按鈕(Demo 用而已)</Button>
+    <div>user prop(demo 用而已): {user || '沒資料'}</div>
   </div>
 );
 
 Index.propTypes = propTypes;
 Index.defaultProps = defaultProps;
+
+Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
 
 export default Index;
