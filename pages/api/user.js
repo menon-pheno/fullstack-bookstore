67bcbf44772169f1095445d7c1b6617d55842ff1
pheno <pheno_the_best@yahoo.com.tw>
2022-03-02 15:57:07 +0800

    chapter 2

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Huang' });
+  res.status(200).json({ email: 'default@bookstore.org' });
 }
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,9 @@
 import Head from 'next/head';
 import Button from '@material-ui/core/Button';
 import PropTypes from 'prop-types';
+import useSWR from 'swr';
+
+import dataFetcher from '../lib/dataFetcher';
 
 const propTypes = {
   user: PropTypes.shape({
@@ -13,21 +16,29 @@ const defaultProps = {
   user: null,
 };
 
-const Index = ({ user }) => (
-  <div style={{ padding: '10px 45px' }}>
-    <Head>
-      <title>首頁</title>
-      <meta name="說明" content="這是首頁的說明資訊" />
-    </Head>
-    <p>首頁資訊</p>
-    <Button variant="contained">MUI 按鈕(Demo 用而已)</Button>
-    <div>user prop(demo 用而已): {user || '沒資料'}</div>
-  </div>
-);
+const Index = ({ user }) => {
+  return (
+    <div style={{ padding: '10px 45px' }}>
+      <Head>
+        <title>首頁</title>
+        <meta name="說明" content="這是首頁的說明資訊" />
+      </Head>
+      <p>首頁資訊</p>
+      <Button variant="contained">MUI 按鈕(Demo 用而已)</Button>
+      <div>user prop(demo 用而已): {user.email || '沒資料'}</div>
+    </div>
+  );
+};
 
 Index.propTypes = propTypes;
 Index.defaultProps = defaultProps;
 
-Index.getInitialProps = async (ctx) => ({ user: ctx.query.user });
+Index.getInitialProps = async () => {
+  return {
+    user: {
+      email: 'default@bookstore.com',
+    },
+  };
+};
 
 export default Index;
