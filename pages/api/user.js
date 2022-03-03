0c0093422d5420a7fb7f945e6b6bbe7b36f544e8
pheno <pheno_the_best@yahoo.com.tw>
2022-03-03 19:55:41 +0800

    get user via mongo

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,3 +1,14 @@
-export default function handler(req, res) {
-  res.status(200).json({ email: 'default2@bookstore.org' });
+import dbConnect from '../../lib/dbConnector';
+import User from '../../models/User';
+
+export default async function handler(req, res) {
+  await dbConnect();
+
+  try {
+    // 假設 MongoDB 裡面有一筆 User 文件，其 slug 欄位的值是 'pheno-author'
+    const user = await User.findOne({ slug: 'pheno-author' });
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 }
