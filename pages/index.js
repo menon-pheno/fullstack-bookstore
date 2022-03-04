ec459ba5e2b8d325b0f5e1b98ec6174b7aebcc90
pheno <pheno_the_best@yahoo.com.tw>
2022-03-04 17:05:22 +0800

    update index

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
-export default function Home() {
-  return <div>我活了！</div>;
-}
+import Head from "next/head";
+
+const Home = () => {
+  return (
+    <div>
+      <Head>
+        <title>首頁</title>
+        <meta name="描述" content="這是關於首頁的描述" />
+      </Head>
+      <p>首頁的內容</p>
+    </div>
+  );
+};
+
+export default Home;
