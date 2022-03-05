bd53386a17b16493e0da2c59e7804dbe79b02bda
John Huang <pheno.the.best@gmail.com>
2022-03-05 18:13:20 +0800

    Header Component

---
+++ pages/test.js
@@ -0,0 +1,12 @@
+import Header from "../components/Header";
+
+const Test = () => {
+    return (
+      <div>
+        <Header />
+        <p>我是個測試頁</p>
+      </div>
+    );
+  };
+
+export default Test;
