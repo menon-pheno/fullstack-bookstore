c4bd3ceb85c1a3322ee095975e610507b3df6a32
John Huang <pheno.the.best@gmail.com>
2022-03-05 11:52:19 +0800

    Header Component

---
+++ components/Header.js
@@ -0,0 +1,5 @@
+const Header = () => {
+    return <div>我是標頭</div>
+};
+
+export default Header;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from "next/head";
 
+import Header from "../components/Header";
+
 const Home = () => {
   return (
     <div>
@@ -7,6 +9,7 @@ const Home = () => {
         <title>首頁</title>
         <meta name="描述" content="這是關於首頁的描述" />
       </Head>
+      <Header />
       <p>首頁的內容</p>
     </div>
   );
