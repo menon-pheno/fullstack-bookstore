357ea89221d963fc2f70a0a958dfcb8e38eb370d
John Huang <pheno.the.best@gmail.com>
2022-03-06 14:07:11 +0800

    Header Component

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
 const Header = () => {
-    return <div>我是標頭</div>
+    return (
+        <>
+            <Link href="/">
+                <a>首頁</a>
+            </Link>
+            <Link href="/test">
+                <a>測試頁</a>
+            </Link>
+        </>
+    )
 };
 
 export default Header;
