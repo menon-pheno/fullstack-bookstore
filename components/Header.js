eb5ccb212d9526819a7d00f33892ad9c77e7e342
John Huang <pheno.the.best@gmail.com>
2022-03-06 17:56:22 +0800

    use fragment; mui integration

--- components/Header.js
+++ components/Header.js
@@ -1,21 +1,22 @@
-import * as React from 'react';
-import Container from '@material-ui/core/Container';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
-//import ProTip from '../src/ProTip';
-//import Link from '../src/Link';
-//import Copyright from '../src/Copyright';
+import Link from "next/link";
 
-export default function Header() {
-  return (
-    <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
-        <Typography variant="h4" component="h1" gutterBottom>
-          Next.js example
-        </Typography>
-        
-        
-      </Box>
-    </Container>
-  );
-}
+import Toolbar from "@mui/material/Toolbar";
+import Grid from "@mui/material/Grid";
+
+const Header = () => {
+    return (
+        <>
+          <Toolbar>
+            <Grid container direction="row" justify="space-around" align="center">
+              <Grid item xs={12} style={{ textAlign: "right" }}>
+                <Link href="/login">
+                  <a>登入</a>
+                </Link>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </>
+    )
+};
+
+export default Header;
