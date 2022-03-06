ffd438c123dd3b2090f80e57dc85174307bde59a
John Huang <pheno.the.best@gmail.com>
2022-03-06 15:32:07 +0800

    new header

--- components/Header.js
+++ components/Header.js
@@ -1,16 +1,22 @@
-import Link from "next/link";
+import Link from 'next/link';
 
-const Header = () => {
-    return (
-        <>
-            <Link href="/">
-                <a>首頁</a>
-            </Link>
-            <Link href="/test">
-                <a>測試頁</a>
-            </Link>
-        </>
-    )
-};
+import Toolbar from '@material-ui/core/Toolbar';
+import Grid from '@material-ui/core/Grid';
+
+//import { styleToolbar } from './SharedStyles';
+
+const Header = () => (
+  <div>
+    <Toolbar>
+      <Grid container direction="row" justify="space-around" align="center">
+        <Grid item xs={12} style={{ textAlign: 'right' }}>
+          <Link href="/login">
+            <a>登入</a>
+          </Link>
+        </Grid>
+      </Grid>
+    </Toolbar>
+  </div>
+);
 
 export default Header;
--- pages/index.js
+++ pages/index.js
@@ -4,14 +4,14 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div>
+    <>
       <Head>
         <title>首頁</title>
         <meta name="描述" content="這是關於首頁的描述" />
       </Head>
       <Header />
       <p>首頁的內容</p>
-    </div>
+    </>
   );
 };
 
