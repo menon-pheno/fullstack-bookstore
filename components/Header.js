3d9f04dca973bafcabf85a17ed0c2f6a074a6f28
pheno <pheno_the_best@yahoo.com.tw>
2022-03-15 13:59:47 +0800

    responsive app bar

--- components/Header.js
+++ components/Header.js
@@ -1,23 +1,7 @@
-import Link from "next/link";
-
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
-import Grid from "@mui/material/Grid";
+import ResponsiveAppBar from "./ResponsiveAppBar";
 
 const Header = () => {
-  return (
-    <>
-      <Toolbar>
-        <Grid container direction="row" justify="space-around" align="center">
-          <Grid item xs={12} style={{ textAlign: "right" }}>
-            <Link href="/login">
-              <a>登入</a>
-            </Link>
-          </Grid>
-        </Grid>
-      </Toolbar>
-    </>
-  );
+  return <ResponsiveAppBar />;
 };
 
 export default Header;
