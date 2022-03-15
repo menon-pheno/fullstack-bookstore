baff8ca5e46813b5a8c75c5ca41f2d35c65e0c9d
pheno <pheno_the_best@yahoo.com.tw>
2022-03-15 18:11:43 +0800

    chapter 2 end

---
+++ book/2-end/components/Header.js
@@ -0,0 +1,20 @@
+import ResponsiveAppBar from "./ResponsiveAppBar";
+
+const Header = () => {
+  const options = [
+    {
+      text: "有問題嗎？",
+      href: "https://github.com/menon-pheno/fullstack-bookstore/issues",
+    },
+    {
+      text: "登出",
+      href: "/logout",
+      anchor: true,
+    },
+  ];
+
+  const navPages = [];
+  return <ResponsiveAppBar options={options} pages={navPages} />;
+};
+
+export default Header;
