ef2f7e98ea47bd6fbc30d3374ba1edcc61d01671
pheno <pheno_the_best@yahoo.com.tw>
2022-03-17 18:59:03 +0800

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -34,6 +34,13 @@ export default function MyApp(props) {
             sizes="16x16"
             href="/favicon-16x16.png"
           />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         </Head>
         <ThemeProvider theme={basicTheme}>
           {/* CssBaseline 會提供一個一致的 CSS 基準線 */}
