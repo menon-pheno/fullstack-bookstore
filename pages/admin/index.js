b74a6e9bee480fd65965c5bd0383aca33d5d8c3b
pheno <pheno_the_best@yahoo.com.tw>
2022-04-27 18:39:20 +0800

    list repos

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -1,11 +1,41 @@
-import { Button } from "@mui/material";
+import { Button, Box, CircularProgress } from "@mui/material";
 import Link from "next/link";
+import useSWR from "swr";
+
+import dataFetcher from "../../lib/dataFetcher";
 
 const Admin = () => {
+  const { data, error } = useSWR(`/api/git-test`, dataFetcher);
+
+  if (error) {
+    return (
+      <>
+        <h1>有錯誤</h1>
+      </>
+    );
+  }
+
+  if (!data) {
+    return (
+      <>
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      </>
+    );
+  }
+
   return (
-    <Link href="/admin/add-book">
-      <Button variant="contained">新增書本</Button>
-    </Link>
+    <>
+      <ul>
+        {data.repos.map((repo) => (
+          <li key={repo}>{repo}</li>
+        ))}
+      </ul>
+      <Link href="/admin/add-book">
+        <Button variant="contained">新增書本</Button>
+      </Link>
+    </>
   );
 };
 
