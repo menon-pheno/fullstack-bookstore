c67d6e3c6c10ae52fff8fbcc24598cc6140ede38
pheno <pheno_the_best@yahoo.com.tw>
2022-05-23 18:56:56 +0800

    chapter list

--- pages/api/books/[...slugs].js
+++ pages/api/books/[...slugs].js
@@ -5,62 +5,118 @@ import Chapter from "../../../models/Chapter";
 async function chapter(req, res) {
   const { slugs } = req.query;
 
-  // 我們希望格式是 api/books/book-slug/chapter-slug 的格式
-  if (slugs.length !== 2) {
-    res
-      .status(400)
-      .json({ success: false, errorMessage: "something wrong with slug" });
-  }
-
+  // slugs.length == 1: api/books/book-slug
+  // slugs.length == 2: api/books/book-slug/chapter-slug
   const { method } = req;
-  const bookSlug = slugs[0];
-  const chapterSlug = slugs[1];
-  await dbConnect();
+  let bookSlug, chapterSlug;
+  switch (slugs.length) {
+    case 1:
+      bookSlug = slugs[0];
+      await dbConnect();
+      switch (method) {
+        case "GET":
+          try {
+            const bookResult = await Book.find({ slug: bookSlug });
+            if (bookResult.length === 0) {
+              res
+                .status(400)
+                .json({ success: false, errorMessage: "No book found" });
+            } else if (bookResult.length > 1) {
+              res.status(400).json({
+                success: false,
+                errorMessage: "More than one book found",
+              });
+            } else {
+              const book = bookResult[0].toObject();
+              book._id = bookResult[0]._id.toString();
 
-  switch (method) {
-    case "GET":
-      try {
-        const bookResult = await Book.find({ slug: bookSlug });
-        if (bookResult.length === 0) {
-          res
-            .status(400)
-            .json({ success: false, errorMessage: "No book found" });
-        } else if (bookResult.length > 1) {
-          res
-            .status(400)
-            .json({ success: false, errorMessage: "More than one book found" });
-        } else {
-          const book = bookResult[0].toObject();
-          book._id = bookResult[0]._id.toString();
-
-          const chapterResult = await Chapter.find({
-            bookId: book._id,
-            slug: chapterSlug,
-          });
-          if (chapterResult.length === 0) {
+              const chapterResult = await Chapter.find({
+                bookId: book._id,
+              });
+              if (chapterResult.length === 0) {
+                res
+                  .status(400)
+                  .json({ success: false, errorMessage: "No chapter found" });
+              } else {
+                const chapters = chapterResult.map((chapter) => {
+                  const processedChapter = chapter.toObject();
+                  processedChapter._id = chapter._id.toString();
+                  return processedChapter;
+                });
+                res.status(200).json({ success: true, chapters });
+              }
+            }
+          } catch (error) {
+            console.log(error);
             res
               .status(400)
-              .json({ success: false, errorMessage: "No chapter found" });
-          } else if (chapterResult.length > 1) {
-            res.status(400).json({
-              success: false,
-              errorMessage: "More than one chapter found",
-            });
-          } else {
-            const chapter = chapterResult[0].toObject();
-            chapter._id = chapterResult[0]._id.toString();
-            res.status(200).json({ success: true, chapter });
+              .json({ success: false, errorMessage: "GET resulted in error" });
           }
-        }
-      } catch (error) {
-        console.log(error);
-        res
-          .status(400)
-          .json({ success: false, errorMessage: "GET resulted in error" });
+          break;
+        default:
+          res
+            .status(400)
+            .json({ success: false, errorMessage: "不該走到這..." });
+          break;
+      }
+      break;
+    case 2:
+      bookSlug = slugs[0];
+      chapterSlug = slugs[1];
+      await dbConnect();
+
+      switch (method) {
+        case "GET":
+          try {
+            const bookResult = await Book.find({ slug: bookSlug });
+            if (bookResult.length === 0) {
+              res
+                .status(400)
+                .json({ success: false, errorMessage: "No book found" });
+            } else if (bookResult.length > 1) {
+              res.status(400).json({
+                success: false,
+                errorMessage: "More than one book found",
+              });
+            } else {
+              const book = bookResult[0].toObject();
+              book._id = bookResult[0]._id.toString();
+
+              const chapterResult = await Chapter.find({
+                bookId: book._id,
+                slug: chapterSlug,
+              });
+              if (chapterResult.length === 0) {
+                res
+                  .status(400)
+                  .json({ success: false, errorMessage: "No chapter found" });
+              } else if (chapterResult.length > 1) {
+                res.status(400).json({
+                  success: false,
+                  errorMessage: "More than one chapter found",
+                });
+              } else {
+                const chapter = chapterResult[0].toObject();
+                chapter._id = chapterResult[0]._id.toString();
+                res.status(200).json({ success: true, chapter });
+              }
+            }
+          } catch (error) {
+            console.log(error);
+            res
+              .status(400)
+              .json({ success: false, errorMessage: "GET resulted in error" });
+          }
+          break;
+        default:
+          res
+            .status(400)
+            .json({ success: false, errorMessage: "不該走到這..." });
       }
       break;
     default:
-      res.status(400).json({ success: false, errorMessage: "不該走到這..." });
+      res.status(400).json({ success: false, errorMessage: "網址有問題" });
+      break;
   }
 }
 
--- pages/books/[bookSlug]/index.js
+++ pages/books/[bookSlug]/index.js
@@ -1,13 +1,48 @@
+import { Box, CircularProgress } from "@mui/material";
 import { useRouter } from "next/router";
+import useSWR from "swr";
+import Link from "next/link";
+
+import dataFetcher from "../../../lib/dataFetcher";
 
 const Book = () => {
   const router = useRouter();
   const { bookSlug } = router.query;
+  const { data, error } = useSWR(
+    bookSlug ? `/api/books/${bookSlug}` : null,
+    dataFetcher
+  );
+
+  if (error) {
+    return (
+      <>
+        <h1>有錯誤</h1>
+      </>
+    );
+  }
+
+  if (!data) {
+    return (
+      <>
+        <Box sx={{ display: "flex" }}>
+          <CircularProgress />
+        </Box>
+      </>
+    );
+  }
 
   return (
     <>
-      <h1>這是書本介紹頁</h1>
-      <h2>書本 slug: {bookSlug}</h2>
+      <h1>書本 slug: {bookSlug}</h1>
+      <ul>
+        {data.chapters.map((chapter) => (
+          <li key={chapter._id}>
+            <Link href={`/books/${bookSlug}/${chapter.slug}`}>
+              <a>{chapter.title}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </>
   );
 };
